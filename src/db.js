4a3c8655e86a6e5a486400908ba9413b08afb551
arg49 <43797904+arg49@users.noreply.github.com>
2021-03-17 23:49:11 +0000

    Gagnagrunnur klár!

---
+++ src/db.js
@@ -0,0 +1,245 @@
+import pg from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const {
+  DATABASE_URL: connectionString,
+  NODE_ENV: nodeEnv = 'development',
+} = process.env;
+
+// Notum SSL tengingu við gagnagrunn ef við erum *ekki* í development mode, þ.e.a.s. á local vél
+const ssl = nodeEnv !== 'development' ? { rejectUnauthorized: false } : false;
+
+const pool = new pg.Pool({ connectionString, ssl });
+
+pool.on('error', (err) => {
+  console.error('Villa í tengingu við gagnagrunn, forrit hættir', err);
+  process.exit(-1);
+});
+
+export async function query(_query, values = []) {
+  const client = await pool.connect();
+
+  try {
+    const result = await client.query(_query, values);
+    return result;
+  } finally {
+    client.release();
+  }
+}
+
+/**
+ * Insert a single registration into the registration table.
+ *
+ * @param {string} entry.name – Name of registrant
+ * @param {string} entry.nationalId – National ID of registrant
+ * @param {string} entry.comment – Comment, if any from registrant
+ * @param {boolean} entry.anonymous – If the registrants name should be displayed or not
+ * @returns {Promise<boolean>} Promise, resolved as true if inserted, otherwise false
+ */
+export async function insert({
+  name, nationalId, comment, anonymous,
+} = {}) {
+  let success = true;
+
+  const q = `
+    INSERT INTO signatures
+      (name, nationalId, comment, anonymous)
+    VALUES
+      ($1, $2, $3, $4);
+  `;
+  const values = [name, nationalId, comment, anonymous === 'on'];
+
+  try {
+    await query(q, values);
+  } catch (e) {
+    console.error('Error inserting signature', e);
+    success = false;
+  }
+
+  return success;
+}
+
+export async function createSeries(data){
+    const q = `
+        INSERT INTO 
+          TVseries (
+            id,
+            showName,
+            releaseDate,
+            stillGoing,
+            tagline,
+            photo,
+            about,
+            language,
+            channel,
+            url)
+          VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
+    `;
+    try {
+        await query(q, [
+            data.id,
+            data.name,
+            data.airDate,
+            data.inProduction,
+            data.tagline,
+            data.image,
+            data.description,
+            data.language,
+            data.network,
+            data.homepage
+        ]);
+    } catch(e) {
+        console.info('Villa við að setja inn series', e);
+    }
+}
+
+export async function createGenre(data){
+    //TODO
+}
+
+export async function createSeasons(data) {
+    const q = `
+        INSERT INTO
+          TVseasons (
+            showName,
+            season_num,
+            releaseDate,
+            about,
+            photo,
+            series_id)
+          VALUES ($1, $2, $3, $4, $5, $6)
+    `;
+    let date = data.airDate;
+    if (date == ''){
+        date = null;
+    }
+    try {
+        await query(q, [
+            data.name,
+            data.number,
+            date,
+            data.overview,
+            data.poster,
+            data.serieId
+        ]);
+    } catch(e) {
+        console.info('Villa við að setja inn seasons', e);
+    }
+}
+
+export async function createEpisodes(data){
+    const q = `
+        INSERT INTO
+          episodes (episodeName, epi_num, releaseDate, about, season, series_id)
+        VALUES ($1, $2, $3, $4, $5, $6)
+    `;
+    let date = data.airDate;
+    if (date == ''){
+        date = null;
+    }
+    try {
+        await query(q, [
+            data.name,
+            data.number,
+            date,
+            data.overview,
+            data.season,
+            data.serieId
+        ]);
+    } catch(e) {
+        console.info('Villa við að setja inn episodes', e);
+    }
+}
+
+/**
+ * List all registrations from the registration table.
+ *
+ * @returns {Promise<Array<list>>} Promise, resolved to array of all registrations.
+ */
+export async function list(offset = 0, limit = 10, search = '') {
+  const values = [offset, limit];
+
+  let searchPart = '';
+  if (search) {
+    searchPart = `
+      WHERE
+      to_tsvector('english', name) @@ plainto_tsquery('english', $3)
+      OR
+      to_tsvector('english', comment) @@ plainto_tsquery('english', $3)
+    `;
+    values.push(search);
+  }
+
+  let result = [];
+
+  try {
+    const q = `
+      SELECT
+        id, name, nationalId, comment, anonymous, signed
+      FROM
+        signatures
+      ${searchPart}
+      ORDER BY signed DESC
+      OFFSET $1 LIMIT $2
+    `;
+
+    const queryResult = await query(q, values);
+
+    if (queryResult && queryResult.rows) {
+      result = queryResult.rows;
+    }
+  } catch (e) {
+    console.error('Error selecting signatures', e);
+  }
+
+  return result;
+}
+
+export async function total(search) {
+  let searchPart = '';
+  if (search) {
+    searchPart = `
+      WHERE
+      to_tsvector('english', name) @@ plainto_tsquery('english', $3)
+      OR
+      to_tsvector('english', comment) @@ plainto_tsquery('english', $3)
+    `;
+  }
+
+  try {
+    const result = await query(
+      `SELECT COUNT(*) AS count FROM signatures ${searchPart}`,
+      search ? [search] : [],
+    );
+    return (result.rows && result.rows[0] && result.rows[0].count) || 0;
+  } catch (e) {
+    console.error('Error counting signatures', e);
+  }
+
+  return 0;
+}
+
+export async function deleteRow(id) {
+  let result = [];
+  try {
+    const queryResult = await query(
+      'DELETE FROM signatures WHERE id = $1',
+      [id],
+    );
+
+    if (queryResult && queryResult.rows) {
+      result = queryResult.rows;
+    }
+  } catch (e) {
+    console.error('Error selecting signatures', e);
+  }
+
+  return result;
+}
+
+// Helper to remove pg from the event loop
+export async function end() {
+  await pool.end();
+}
